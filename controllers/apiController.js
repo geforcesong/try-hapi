01f405a914e0a73635e82a02f94997c55abe0491
gguo <gguo@movoto.com>
2018-08-27 16:04:23 -0700

    updated ssr validation

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,18 @@
+var request = require('request');
+
 class ApiController {
-    async loadView(request, h) {
-        return request.payload;
+    async loadView(req, h) {
+
+        return new Promise((resolve, reject) => {
+            request.post({
+                headers: { 'content-type': 'application/x-www-form-urlencoded; charset=utf-8' },
+                url: 'https://www.google.com/recaptcha/api/siteverify',
+                body: `response=${req.payload['g-recaptcha-response']}&secret=6LefwWwUAAAAAGNljmD2WPK9oh-YSb2OkKaLRzov`
+            }, function (error, response, body) {
+                console.log(body);
+                return resolve(body);
+            });
+        });
     }
 }
 
